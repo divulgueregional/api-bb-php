bf32d35bbb037156b2782a45ea4b95b7865ca319
divulgueregional <roseno@divulgueregional.com.br>
2023-03-23 20:18:18 -0300

    arquivo exemplo para gearr o pdf do boleto

--- docs/COBRANÇAS/Boleto Em PDF/index.php
+++ docs/COBRANÇAS/Boleto Em PDF/index.php
@@ -1,113 +1,9 @@
 <?php
-
-require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';//domPDF
 require_once 'bb/PDFBoleto/BBBoletoPDF.php';
 
 
-//EXEMPLO DE PDF COM DADOS RETORNADOS DA API
-
-/*
-
-$dadosRetorno = $resp['response'];
-
-$dadosBoleto = new stdClass();
-$dadosBoleto->nossoNumero = '000000000';
-$dadosBoleto->agencia = '000000000';
-$dadosBoleto->codigoCedente = '000000000';
-$dadosBoleto->gerarDuasVias = true;
-//---
-$dadosBoleto->codigoLinhaDigitavel = $dadosRetorno->codigoLinhaDigitavel;
-$dadosBoleto->textoEmailPagador = '';
-$dadosBoleto->textoMensagemBloquetoTitulo = $dadosRetorno->textoMensagemBloquetoTitulo;
-$dadosBoleto->codigoTipoMulta = $dadosRetorno->codigoTipoMulta;
-$dadosBoleto->codigoCanalPagamento = $dadosRetorno->codigoCanalPagamento;
-$dadosBoleto->numeroContratoCobranca = $dadosRetorno->numeroContratoCobranca;
-$dadosBoleto->codigoTipoInscricaoSacado = $dadosRetorno->codigoTipoInscricaoSacado;
-$dadosBoleto->numeroInscricaoSacadoCobranca = $dadosRetorno->numeroInscricaoSacadoCobranca;
-$dadosBoleto->codigoEstadoTituloCobranca = $dadosRetorno->codigoEstadoTituloCobranca;
-$dadosBoleto->codigoTipoTituloCobranca = $dadosRetorno->codigoTipoTituloCobranca;
-$dadosBoleto->codigoModalidadeTitulo = $dadosRetorno->codigoModalidadeTitulo;
-$dadosBoleto->codigoAceiteTituloCobranca = $dadosRetorno->codigoAceiteTituloCobranca;
-$dadosBoleto->codigoPrefixoDependenciaCobrador = $dadosRetorno->codigoPrefixoDependenciaCobrador;
-$dadosBoleto->codigoIndicadorEconomico = $dadosRetorno->codigoIndicadorEconomico;
-$dadosBoleto->numeroTituloCedenteCobranca = $dadosRetorno->numeroTituloCedenteCobranca;
-$dadosBoleto->codigoTipoJuroMora = $dadosRetorno->codigoTipoJuroMora;
-$dadosBoleto->dataEmissaoTituloCobranca = str_replace('.', '/', $dadosRetorno->dataEmissaoTituloCobranca);
-$dadosBoleto->dataRegistroTituloCobranca = str_replace('.', '/', $dadosRetorno->dataRegistroTituloCobranca);
-$dadosBoleto->dataVencimentoTituloCobranca = str_replace('.', '/', $dadosRetorno->dataVencimentoTituloCobranca);
-$dadosBoleto->valorOriginalTituloCobranca = $dadosRetorno->valorOriginalTituloCobranca;
-$dadosBoleto->valorAtualTituloCobranca = $dadosRetorno->valorAtualTituloCobranca;
-$dadosBoleto->valorPagamentoParcialTitulo = $dadosRetorno->valorPagamentoParcialTitulo;
-$dadosBoleto->valorAbatimentoTituloCobranca = $dadosRetorno->valorAbatimentoTituloCobranca;
-$dadosBoleto->percentualImpostoSobreOprFinanceirasTituloCobranca = $dadosRetorno->percentualImpostoSobreOprFinanceirasTituloCobranca;
-$dadosBoleto->valorImpostoSobreOprFinanceirasTituloCobranca = $dadosRetorno->valorImpostoSobreOprFinanceirasTituloCobranca;
-$dadosBoleto->valorMoedaTituloCobranca = $dadosRetorno->valorMoedaTituloCobranca;
-$dadosBoleto->percentualJuroMoraTitulo = $dadosRetorno->percentualJuroMoraTitulo;
-$dadosBoleto->valorJuroMoraTitulo = $dadosRetorno->valorJuroMoraTitulo;
-$dadosBoleto->percentualMultaTitulo = $dadosRetorno->percentualMultaTitulo;
-$dadosBoleto->valorMultaTituloCobranca = $dadosRetorno->valorMultaTituloCobranca;
-$dadosBoleto->quantidadeParcelaTituloCobranca = $dadosRetorno->quantidadeParcelaTituloCobranca;
-$dadosBoleto->dataBaixaAutomaticoTitulo = str_replace('.', '/', $dadosRetorno->dataBaixaAutomaticoTitulo);
-$dadosBoleto->textoCampoUtilizacaoCedente = $dadosRetorno->textoCampoUtilizacaoCedente;
-$dadosBoleto->indicadorCobrancaPartilhadoTitulo = $dadosRetorno->indicadorCobrancaPartilhadoTitulo;
-$dadosBoleto->nomeSacadoCobranca = $dadosRetorno->nomeSacadoCobranca;
-$dadosBoleto->textoEnderecoSacadoCobranca = $dadosRetorno->textoEnderecoSacadoCobranca;
-$dadosBoleto->nomeBairroSacadoCobranca = $dadosRetorno->nomeBairroSacadoCobranca;
-$dadosBoleto->nomeMunicipioSacadoCobranca = $dadosRetorno->nomeMunicipioSacadoCobranca;
-$dadosBoleto->siglaUnidadeFederacaoSacadoCobranca = $dadosRetorno->siglaUnidadeFederacaoSacadoCobranca;
-$dadosBoleto->numeroCepSacadoCobranca = $dadosRetorno->numeroCepSacadoCobranca;
-$dadosBoleto->valorMoedaAbatimentoTitulo = $dadosRetorno->valorMoedaAbatimentoTitulo;
-$dadosBoleto->dataProtestoTituloCobranca = $dadosRetorno->dataProtestoTituloCobranca;
-$dadosBoleto->codigoTipoInscricaoSacador = $dadosRetorno->codigoTipoInscricaoSacador;
-$dadosBoleto->numeroInscricaoSacadorAvalista = $dadosRetorno->numeroInscricaoSacadorAvalista;
-$dadosBoleto->nomeSacadorAvalistaTitulo = $dadosRetorno->nomeSacadorAvalistaTitulo;
-$dadosBoleto->percentualDescontoTitulo = $dadosRetorno->percentualDescontoTitulo;
-$dadosBoleto->dataDescontoTitulo = $dadosRetorno->dataDescontoTitulo;
-$dadosBoleto->valorDescontoTitulo = $dadosRetorno->valorDescontoTitulo;
-$dadosBoleto->codigoDescontoTitulo = $dadosRetorno->codigoDescontoTitulo;
-$dadosBoleto->percentualSegundoDescontoTitulo = $dadosRetorno->percentualSegundoDescontoTitulo;
-$dadosBoleto->dataSegundoDescontoTitulo = $dadosRetorno->dataSegundoDescontoTitulo;
-$dadosBoleto->valorSegundoDescontoTitulo = $dadosRetorno->valorSegundoDescontoTitulo;
-$dadosBoleto->codigoSegundoDescontoTitulo = $dadosRetorno->codigoSegundoDescontoTitulo;
-$dadosBoleto->percentualTerceiroDescontoTitulo = $dadosRetorno->percentualTerceiroDescontoTitulo;
-$dadosBoleto->dataTerceiroDescontoTitulo = $dadosRetorno->dataTerceiroDescontoTitulo;
-$dadosBoleto->valorTerceiroDescontoTitulo = $dadosRetorno->valorTerceiroDescontoTitulo;
-$dadosBoleto->codigoTerceiroDescontoTitulo = $dadosRetorno->codigoTerceiroDescontoTitulo;
-$dadosBoleto->dataMultaTitulo = $dadosRetorno->dataMultaTitulo;
-$dadosBoleto->numeroCarteiraCobranca = $dadosRetorno->numeroCarteiraCobranca;
-$dadosBoleto->numeroVariacaoCarteiraCobranca = $dadosRetorno->numeroVariacaoCarteiraCobranca;
-$dadosBoleto->quantidadeDiaProtesto = $dadosRetorno->quantidadeDiaProtesto;
-$dadosBoleto->quantidadeDiaPrazoLimiteRecebimento = $dadosRetorno->quantidadeDiaPrazoLimiteRecebimento;
-$dadosBoleto->dataLimiteRecebimentoTitulo = str_replace('.', '/', $dadosRetorno->dataLimiteRecebimentoTitulo);
-$dadosBoleto->indicadorPermissaoRecebimentoParcial = $dadosRetorno->indicadorPermissaoRecebimentoParcial;
-$dadosBoleto->textoCodigoBarrasTituloCobranca = $dadosRetorno->textoCodigoBarrasTituloCobranca;
-$dadosBoleto->codigoOcorrenciaCartorio = $dadosRetorno->codigoOcorrenciaCartorio;
-$dadosBoleto->valorImpostoSobreOprFinanceirasRecebidoTitulo = $dadosRetorno->valorImpostoSobreOprFinanceirasRecebidoTitulo;
-$dadosBoleto->valorAbatimentoTotal = $dadosRetorno->valorAbatimentoTotal;
-$dadosBoleto->valorJuroMoraRecebido = $dadosRetorno->valorJuroMoraRecebido;
-$dadosBoleto->valorDescontoUtilizado = $dadosRetorno->valorDescontoUtilizado;
-$dadosBoleto->valorPagoSacado = $dadosRetorno->valorPagoSacado;
-$dadosBoleto->valorCreditoCedente = $dadosRetorno->valorCreditoCedente;
-$dadosBoleto->codigoTipoLiquidacao = $dadosRetorno->codigoTipoLiquidacao;
-$dadosBoleto->dataCreditoLiquidacao = $dadosRetorno->dataCreditoLiquidacao;
-$dadosBoleto->dataRecebimentoTitulo = $dadosRetorno->dataRecebimentoTitulo;
-$dadosBoleto->codigoPrefixoDependenciaRecebedor = $dadosRetorno->codigoPrefixoDependenciaRecebedor;
-$dadosBoleto->codigoNaturezaRecebimento = $dadosRetorno->codigoNaturezaRecebimento;
-$dadosBoleto->numeroIdentidadeSacadoTituloCobranca = $dadosRetorno->numeroIdentidadeSacadoTituloCobranca;
-$dadosBoleto->codigoResponsavelAtualizacao = $dadosRetorno->codigoResponsavelAtualizacao;
-$dadosBoleto->codigoTipoBaixaTitulo = $dadosRetorno->codigoTipoBaixaTitulo;
-$dadosBoleto->valorMultaRecebido = $dadosRetorno->valorMultaRecebido;
-$dadosBoleto->valorReajuste = $dadosRetorno->valorReajuste;
-$dadosBoleto->valorOutroRecebido = $dadosRetorno->valorOutroRecebido;
-$dadosBoleto->codigoIndicadorEconomicoUtilizadoInadimplencia = $dadosRetorno->codigoIndicadorEconomicoUtilizadoInadimplencia;
-*/
-
-
-
-
 //EXEMPLO DE PDF COM DADOS MANUAIS
-
-
 $boletoPDF = new BBBoletoPDF($dadosBoleto);
 $dadosBoleto = new stdClass();
 $dadosBoleto->nossoNumero = '0000011';
@@ -206,3 +102,101 @@ $pdf = $boleto->getPDF();
 
 header("Content-type:application/pdf");
 echo $pdf;
+
+//EXEMPLO DE PDF COM DADOS RETORNADOS DA API
+
+/*
+
+$dadosRetorno = $resp['response'];
+
+$dadosBoleto = new stdClass();
+$dadosBoleto->nossoNumero = '000000000';
+$dadosBoleto->agencia = '000000000';
+$dadosBoleto->codigoCedente = '000000000';
+$dadosBoleto->gerarDuasVias = true;
+//---
+$dadosBoleto->codigoLinhaDigitavel = $dadosRetorno->codigoLinhaDigitavel;
+$dadosBoleto->textoEmailPagador = '';
+$dadosBoleto->textoMensagemBloquetoTitulo = $dadosRetorno->textoMensagemBloquetoTitulo;
+$dadosBoleto->codigoTipoMulta = $dadosRetorno->codigoTipoMulta;
+$dadosBoleto->codigoCanalPagamento = $dadosRetorno->codigoCanalPagamento;
+$dadosBoleto->numeroContratoCobranca = $dadosRetorno->numeroContratoCobranca;
+$dadosBoleto->codigoTipoInscricaoSacado = $dadosRetorno->codigoTipoInscricaoSacado;
+$dadosBoleto->numeroInscricaoSacadoCobranca = $dadosRetorno->numeroInscricaoSacadoCobranca;
+$dadosBoleto->codigoEstadoTituloCobranca = $dadosRetorno->codigoEstadoTituloCobranca;
+$dadosBoleto->codigoTipoTituloCobranca = $dadosRetorno->codigoTipoTituloCobranca;
+$dadosBoleto->codigoModalidadeTitulo = $dadosRetorno->codigoModalidadeTitulo;
+$dadosBoleto->codigoAceiteTituloCobranca = $dadosRetorno->codigoAceiteTituloCobranca;
+$dadosBoleto->codigoPrefixoDependenciaCobrador = $dadosRetorno->codigoPrefixoDependenciaCobrador;
+$dadosBoleto->codigoIndicadorEconomico = $dadosRetorno->codigoIndicadorEconomico;
+$dadosBoleto->numeroTituloCedenteCobranca = $dadosRetorno->numeroTituloCedenteCobranca;
+$dadosBoleto->codigoTipoJuroMora = $dadosRetorno->codigoTipoJuroMora;
+$dadosBoleto->dataEmissaoTituloCobranca = str_replace('.', '/', $dadosRetorno->dataEmissaoTituloCobranca);
+$dadosBoleto->dataRegistroTituloCobranca = str_replace('.', '/', $dadosRetorno->dataRegistroTituloCobranca);
+$dadosBoleto->dataVencimentoTituloCobranca = str_replace('.', '/', $dadosRetorno->dataVencimentoTituloCobranca);
+$dadosBoleto->valorOriginalTituloCobranca = $dadosRetorno->valorOriginalTituloCobranca;
+$dadosBoleto->valorAtualTituloCobranca = $dadosRetorno->valorAtualTituloCobranca;
+$dadosBoleto->valorPagamentoParcialTitulo = $dadosRetorno->valorPagamentoParcialTitulo;
+$dadosBoleto->valorAbatimentoTituloCobranca = $dadosRetorno->valorAbatimentoTituloCobranca;
+$dadosBoleto->percentualImpostoSobreOprFinanceirasTituloCobranca = $dadosRetorno->percentualImpostoSobreOprFinanceirasTituloCobranca;
+$dadosBoleto->valorImpostoSobreOprFinanceirasTituloCobranca = $dadosRetorno->valorImpostoSobreOprFinanceirasTituloCobranca;
+$dadosBoleto->valorMoedaTituloCobranca = $dadosRetorno->valorMoedaTituloCobranca;
+$dadosBoleto->percentualJuroMoraTitulo = $dadosRetorno->percentualJuroMoraTitulo;
+$dadosBoleto->valorJuroMoraTitulo = $dadosRetorno->valorJuroMoraTitulo;
+$dadosBoleto->percentualMultaTitulo = $dadosRetorno->percentualMultaTitulo;
+$dadosBoleto->valorMultaTituloCobranca = $dadosRetorno->valorMultaTituloCobranca;
+$dadosBoleto->quantidadeParcelaTituloCobranca = $dadosRetorno->quantidadeParcelaTituloCobranca;
+$dadosBoleto->dataBaixaAutomaticoTitulo = str_replace('.', '/', $dadosRetorno->dataBaixaAutomaticoTitulo);
+$dadosBoleto->textoCampoUtilizacaoCedente = $dadosRetorno->textoCampoUtilizacaoCedente;
+$dadosBoleto->indicadorCobrancaPartilhadoTitulo = $dadosRetorno->indicadorCobrancaPartilhadoTitulo;
+$dadosBoleto->nomeSacadoCobranca = $dadosRetorno->nomeSacadoCobranca;
+$dadosBoleto->textoEnderecoSacadoCobranca = $dadosRetorno->textoEnderecoSacadoCobranca;
+$dadosBoleto->nomeBairroSacadoCobranca = $dadosRetorno->nomeBairroSacadoCobranca;
+$dadosBoleto->nomeMunicipioSacadoCobranca = $dadosRetorno->nomeMunicipioSacadoCobranca;
+$dadosBoleto->siglaUnidadeFederacaoSacadoCobranca = $dadosRetorno->siglaUnidadeFederacaoSacadoCobranca;
+$dadosBoleto->numeroCepSacadoCobranca = $dadosRetorno->numeroCepSacadoCobranca;
+$dadosBoleto->valorMoedaAbatimentoTitulo = $dadosRetorno->valorMoedaAbatimentoTitulo;
+$dadosBoleto->dataProtestoTituloCobranca = $dadosRetorno->dataProtestoTituloCobranca;
+$dadosBoleto->codigoTipoInscricaoSacador = $dadosRetorno->codigoTipoInscricaoSacador;
+$dadosBoleto->numeroInscricaoSacadorAvalista = $dadosRetorno->numeroInscricaoSacadorAvalista;
+$dadosBoleto->nomeSacadorAvalistaTitulo = $dadosRetorno->nomeSacadorAvalistaTitulo;
+$dadosBoleto->percentualDescontoTitulo = $dadosRetorno->percentualDescontoTitulo;
+$dadosBoleto->dataDescontoTitulo = $dadosRetorno->dataDescontoTitulo;
+$dadosBoleto->valorDescontoTitulo = $dadosRetorno->valorDescontoTitulo;
+$dadosBoleto->codigoDescontoTitulo = $dadosRetorno->codigoDescontoTitulo;
+$dadosBoleto->percentualSegundoDescontoTitulo = $dadosRetorno->percentualSegundoDescontoTitulo;
+$dadosBoleto->dataSegundoDescontoTitulo = $dadosRetorno->dataSegundoDescontoTitulo;
+$dadosBoleto->valorSegundoDescontoTitulo = $dadosRetorno->valorSegundoDescontoTitulo;
+$dadosBoleto->codigoSegundoDescontoTitulo = $dadosRetorno->codigoSegundoDescontoTitulo;
+$dadosBoleto->percentualTerceiroDescontoTitulo = $dadosRetorno->percentualTerceiroDescontoTitulo;
+$dadosBoleto->dataTerceiroDescontoTitulo = $dadosRetorno->dataTerceiroDescontoTitulo;
+$dadosBoleto->valorTerceiroDescontoTitulo = $dadosRetorno->valorTerceiroDescontoTitulo;
+$dadosBoleto->codigoTerceiroDescontoTitulo = $dadosRetorno->codigoTerceiroDescontoTitulo;
+$dadosBoleto->dataMultaTitulo = $dadosRetorno->dataMultaTitulo;
+$dadosBoleto->numeroCarteiraCobranca = $dadosRetorno->numeroCarteiraCobranca;
+$dadosBoleto->numeroVariacaoCarteiraCobranca = $dadosRetorno->numeroVariacaoCarteiraCobranca;
+$dadosBoleto->quantidadeDiaProtesto = $dadosRetorno->quantidadeDiaProtesto;
+$dadosBoleto->quantidadeDiaPrazoLimiteRecebimento = $dadosRetorno->quantidadeDiaPrazoLimiteRecebimento;
+$dadosBoleto->dataLimiteRecebimentoTitulo = str_replace('.', '/', $dadosRetorno->dataLimiteRecebimentoTitulo);
+$dadosBoleto->indicadorPermissaoRecebimentoParcial = $dadosRetorno->indicadorPermissaoRecebimentoParcial;
+$dadosBoleto->textoCodigoBarrasTituloCobranca = $dadosRetorno->textoCodigoBarrasTituloCobranca;
+$dadosBoleto->codigoOcorrenciaCartorio = $dadosRetorno->codigoOcorrenciaCartorio;
+$dadosBoleto->valorImpostoSobreOprFinanceirasRecebidoTitulo = $dadosRetorno->valorImpostoSobreOprFinanceirasRecebidoTitulo;
+$dadosBoleto->valorAbatimentoTotal = $dadosRetorno->valorAbatimentoTotal;
+$dadosBoleto->valorJuroMoraRecebido = $dadosRetorno->valorJuroMoraRecebido;
+$dadosBoleto->valorDescontoUtilizado = $dadosRetorno->valorDescontoUtilizado;
+$dadosBoleto->valorPagoSacado = $dadosRetorno->valorPagoSacado;
+$dadosBoleto->valorCreditoCedente = $dadosRetorno->valorCreditoCedente;
+$dadosBoleto->codigoTipoLiquidacao = $dadosRetorno->codigoTipoLiquidacao;
+$dadosBoleto->dataCreditoLiquidacao = $dadosRetorno->dataCreditoLiquidacao;
+$dadosBoleto->dataRecebimentoTitulo = $dadosRetorno->dataRecebimentoTitulo;
+$dadosBoleto->codigoPrefixoDependenciaRecebedor = $dadosRetorno->codigoPrefixoDependenciaRecebedor;
+$dadosBoleto->codigoNaturezaRecebimento = $dadosRetorno->codigoNaturezaRecebimento;
+$dadosBoleto->numeroIdentidadeSacadoTituloCobranca = $dadosRetorno->numeroIdentidadeSacadoTituloCobranca;
+$dadosBoleto->codigoResponsavelAtualizacao = $dadosRetorno->codigoResponsavelAtualizacao;
+$dadosBoleto->codigoTipoBaixaTitulo = $dadosRetorno->codigoTipoBaixaTitulo;
+$dadosBoleto->valorMultaRecebido = $dadosRetorno->valorMultaRecebido;
+$dadosBoleto->valorReajuste = $dadosRetorno->valorReajuste;
+$dadosBoleto->valorOutroRecebido = $dadosRetorno->valorOutroRecebido;
+$dadosBoleto->codigoIndicadorEconomicoUtilizadoInadimplencia = $dadosRetorno->codigoIndicadorEconomicoUtilizadoInadimplencia;
+*/
